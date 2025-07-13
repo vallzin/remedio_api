eba6100595d5b2bfbf1c769f1feeeea064f5640e
vallzin <valmilson@gmail.com>
2025-07-13 18:03:52 -0300

    iniciando commits

---
+++ src/main/java/com/remedio_api/RemedioApiApplication.java
@@ -0,0 +1,13 @@
+package com.remedio_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemedioApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RemedioApiApplication.class, args);
+	}
+
+}
